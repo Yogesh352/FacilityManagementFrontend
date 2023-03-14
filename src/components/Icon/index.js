c74adc6f8654268fa32f5a52e7db21db9de74147
Yogesh352 <80184454+Yogesh352@users.noreply.github.com>
2023-03-14 13:33:25 +0800

    layout

---
+++ src/components/Icon/index.js
@@ -0,0 +1,43 @@
+import {
+    HiOutlineTemplate,
+    HiOutlineClipboardList,
+    HiOutlineShieldCheck,
+    HiOutlineChatAlt2,
+    HiMenu,
+    HiBell,
+    HiOutlineUserCircle,
+    HiPencil,
+    HiExternalLink,
+    HiPaperClip,
+  } from "react-icons/hi";
+  import { MdOutlineCancel } from "react-icons/md";
+  import { FiChevronLeft } from "react-icons/fi";
+  
+  import { IoMdContacts } from "react-icons/io";
+  
+  import {
+    AiOutlineCalendar,
+    AiOutlineMenuFold,
+    AiOutlineMenuUnfold,
+  } from "react-icons/ai";
+  
+  
+  export const DashboardIcon = HiOutlineTemplate;
+  export const LeftIcon = FiChevronLeft;
+  export const ClipboardIcon = HiOutlineClipboardList;
+  export const TodoIcon = HiOutlineShieldCheck;
+  export const ChatIcon = HiOutlineChatAlt2;
+  export const MenuIcon = HiMenu;
+  export const AccountIcon = HiOutlineUserCircle;
+  export const NotificationIcon = HiBell;
+  export const CancelIcon = MdOutlineCancel;
+  export const CalendarIcon = AiOutlineCalendar;
+  export const ContactsIcon = IoMdContacts;
+  export const MenufoldIcon = AiOutlineMenuFold;
+  export const MenuunfoldIcon = AiOutlineMenuUnfold;
+  export const EditIcon = HiPencil;
+//   export const DropdownMenuIcon = IconDots;
+//   export const DeleteIcon = IconTrash;
+  export const ExpandIcon = HiExternalLink;
+  export const AttachmentIcon = HiPaperClip;
+  
