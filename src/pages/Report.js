65b80f10c8c1631ec68bc8a24a7a4abdc05a51f7
ynseah.2021 <ynseah.2021@smu.edu.sg>
2023-03-18 15:02:37 +0800

    Added Report page to SideBar and Created Page

---
+++ src/pages/Report.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Stack, Title} from '@mantine/core';
+
+const Report = () => {
+    return (
+        <Stack>
+            <Title className="text-black m-3 mt-4 font-semibold">
+                Summary Reports
+            </Title>
+            
+        </Stack>
+    )
+}
+
+export default Report
