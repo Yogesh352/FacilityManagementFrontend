a984f5396e73d61aa69580ff54ef82f6e6daa70d
liqipotato <89569278+liqipotato@users.noreply.github.com>
2023-03-18 15:16:42 +0800

    rule customization

---
+++ src/pages/FacilitiesCustomization.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FacilitiesCustomization = () => {
+    return (
+        <div>
+            Factilities Customization
+        </div>
+    )
+}
+
+export default FacilitiesCustomization
