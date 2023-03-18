e9d320972d8ceffda579864a91a8870b08f311ee
ynseah.2021 <ynseah.2021@smu.edu.sg>
2023-03-18 15:58:19 +0800

    edited summary report

--- src/pages/SummaryReport.js
+++ src/pages/SummaryReport.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import {Stack, Title, Text, Grid} from '@mantine/core';
-import { useState } from 'react';
 
 const [value, setValue] = useState<[Date | null, Date | null]>([null, null]);
 
 const SummaryReport = () => {
     return (
-        <Stack>
-            <Title className="text-black m-3 mt-4 font-semibold">
-                Summary Reports
-            </Title>
+        // <Stack>
+        //     <Title className="text-black m-3 mt-4 font-semibold">
+        //         Summary Reports
+        //     </Title>
             
-            <Grid>
-                <Grid.Col span={4}>
-                    Filter
-                </Grid.Col>
+        //     <Grid>
+        //         <Grid.Col span={4}>
+        //             Filter
+        //         </Grid.Col>
                 
-                <Grid.Col span={2} offset={5}>
-                    Button
-                </Grid.Col>
-            </Grid>
+        //         <Grid.Col span={2} offset={5}>
+        //             Button
+        //         </Grid.Col>
+        //     </Grid>
 
-        </Stack>
-
+        // </Stack>
+        
+        <div>
+            summary
+        </div>
     )
 }
 
